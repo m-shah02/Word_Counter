ba49ee4f29b427e8d76a29eccb7afb78f407e852
m-shah02 <mansoorshah488@gmail.com>
2023-12-26 00:12:07 +0000

    Improved documentation

--- src/Word_Counter/WordCounterClient.java
+++ src/Word_Counter/WordCounterClient.java
@@ -1,32 +1,45 @@
-/**
- * Client class of the Word Counter application
- * 
- * This is the class where the once the client has established a connection to Server can provide its sample text
- * After which it will await a response from the server for how many words are in the sample text * 
- * 
- * @author Mansoor Shah
- */
 package Word_Counter;
 
 import java.io.*;
 import java.util.Scanner;
-
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
+/**
+ * Client class of the Word Counter application.
+ * 
+ * This class represents the client-side functionality of the Word Counter application.
+ * It establishes a connection to the server and allows the user to input text, 
+ * receiving word count responses from the server. The client uses SSL/TLS for secure 
+ * communication with the server.
+ * 
+ * Upon establishing a connection, the client sends messages to the server for word 
+ * counting based on user input. The program continues to receive word count responses 
+ * until the user decides to exit by typing 'exit'.
+ * 
+ * The SSL/TLS configuration for client-side communication is set up by specifying 
+ * truststore information, enabling specific SSL protocols, and performing an SSL handshake.
+ * 
+ * @author Mansoor Shah
+ */
 public class WordCounterClient {
 	
 	static int portNumber = 17777;
+	// Directory and password for truststore
 	static final String TRUSTSTOREPATH = "src\\keys\\client.truststore";
 	static final String TRUSTSTOREPASS = "tpJ585GQ";
 	
 	/**
-	 * The main method where the client connects to the server.
-	 * Then client performs the required task of providing sample text 
-	 * for server to count the words of.
-	 * 
-	 * @param args
-	 * @exception IOException
+	 * Initiates a client-side connection using SSL to communicate with a server.
+	 * Establishes a connection to the server using SSLSocket and communicates
+	 * by sending messages and receiving word counts for input messages until
+	 * the user exits.
+	 *
+	 * Uses SSL/TLS to connect securely to the server. The client-side SSL
+	 * configuration is set up by specifying truststore information, enabling
+	 * specific SSL protocols, and performing an SSL handshake.
+	 *
+	 * @param args The command-line arguments passed to the program.
 	 */
     public static void main(String[] args) {
         try {
@@ -44,11 +57,15 @@ public class WordCounterClient {
             s.setEnabledProtocols(new String[] {"TLSv1.3", "TLSv1.2"});
             s.startHandshake(); // the SSL handshake
             
+            // Sends message to say there's been a successful connection to the server
             System.out.println("Connected to server.");
 
+            // Used to create buffered reader that reads data from server
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            // Used to create a PrintWriter that writes data to server
             PrintWriter out = new PrintWriter(s.getOutputStream(), true);
 
+            // For the inputted message to scan
             Scanner scanner = new Scanner(System.in);
             String userInput;
             do {
--- src/Word_Counter/WordCounterServer.java
+++ src/Word_Counter/WordCounterServer.java
@@ -1,26 +1,49 @@
-/**
- * This class is for the server that the client interacts with and gets a response from...
- * Provides answer to the input of how many words in the sample text once sent from the client
- * Server responds back with the answer 
- * 
- */
 package Word_Counter;
 
 import java.io.*;
 import java.net.*;
-
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 
+/**
+ * The WordCounterServer class represents a server application for the Word Counter system.
+ * 
+ * This class initializes a server to handle client connections using SSL/TLS for secure
+ * communication. It listens for incoming client connections and creates a separate thread
+ * (ClientHandler) for each client to manage individual client-server communication. The server
+ * continues to accept client connections indefinitely, handling multiple clients concurrently.
+ * 
+ * The server-side SSL configuration is implemented by specifying the keystore location
+ * and password. It utilizes SSLServerSocketFactory to create a secure server socket for
+ * communication with clients over SSL/TLS.
+ * 
+ * @author Mansoor Shah
+ */
 public class WordCounterServer {
 	
+	// Directory and password for keystores
 	static final String KEYSTOREPATH = "src\\keys\\wc.jks";
 	static final String KEYSTOREPASS = "tpJ585GQ";
 	
 	/**
-	 * Main method for Generating server and creating threads to allow for multiple connections
+	 * Main method representing the server's entry point to handle client connections.
 	 * 
-	 * @param args
+	 * The main method initializes and starts the server-side application for handling client
+	 * connections. It sets up the server to listen for incoming client connections and creates
+	 * a new thread (ClientHandler) for each client to manage individual client-server communication.
+	 * The server runs indefinitely, continuously accepting new client connections and creating
+	 * separate threads to handle them.
+	 * 
+	 * Server-side SSL is implemented by specifying the keystore location and password for the
+	 * SSL configuration. It uses SSLServerSocketFactory to create a secure server socket for
+	 * communication with clients over SSL/TLS.
+	 * 
+	 * Upon accepting a client connection, the server displays a message indicating the client's
+	 * IP address and port and initiates a new thread (ClientHandler) to handle communication with
+	 * the connected client. Each client is managed in a separate thread, enabling multiple clients
+	 * to connect and communicate concurrently.
+	 * 
+	 * @param args The command-line arguments passed to the program.
 	 */
     public static void main(String[] args) {
         try {
@@ -58,26 +81,49 @@ public class WordCounterServer {
     }
     
     /**
-     * Inner class that provides the response back to the client
-     * @author Mansoor Shah
+     * Inner class representing a ClientHandler to manage client-server communication.
+     * 
+     * The ClientHandler class implements the Runnable interface to handle client requests
+     * within a server thread. It manages communication with a specific client by reading
+     * incoming messages, processing them to determine word counts, and sending back the calculated
+     * word count to the client. The class continues to handle communication until the client sends
+     * an "exit" message, at which point it closes the communication streams and disconnects
+     * the client from the server.
      *
+     * ClientHandler retrieves the client's IP address and port for identification and logs
+     * incoming messages along with the respective client details. Upon receiving an "exit" message
+     * from the client, it terminates the communication session and closes the associated
+     * input/output streams and the client socket.
      */
     static class ClientHandler implements Runnable {
         private final Socket clientSocket;
         
         /**
-         * Constructor method
+         * Constructs a ClientHandler object to manage client-server communication.
          * 
-         * @param socket
+         * Initializes a ClientHandler object with the provided Socket representing a
+         * connection to a client. This constructor sets up the instance to handle incoming
+         * communication from the associated client socket.
+         *
+         * @param socket The Socket object representing the client connection.
          */
         public ClientHandler(Socket socket) {
             this.clientSocket = socket;
         }
         
         /**
-         * Entry point for the thread which performs actions to handle the client's request
+         * Overrides the 'run' method of the Runnable interface to handle client-server communication.
          * 
-         * @exception IOException
+         * This method represents the execution logic for handling client requests within a server thread.
+         * It reads incoming messages from the client, processes them to determine word counts,
+         * and sends the calculated word count back to the client. The method continues to receive 
+         * and process messages until the client sends an "exit" message, at which point it closes
+         * the communication streams and disconnects the client from the server.
+         *
+         * The method retrieves the client's IP address and port for identification and logs 
+         * incoming messages with their respective client details. Upon receiving an "exit" message
+         * from the client, it terminates the communication session and closes the associated
+         * input/output streams and the client socket.
          */
         @Override
         public void run() {
@@ -87,7 +133,9 @@ public class WordCounterServer {
                 String clientAddress = clientSocket.getInetAddress().getHostAddress();
                 int clientPort = clientSocket.getPort();
                 
+                // Used to create buffered reader that reads data from client
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                // Used to create a PrintWriter that writes data to client
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
                 String inputLine;
@@ -112,11 +160,17 @@ public class WordCounterServer {
                 e.printStackTrace();
             }
         }
+        
         /**
-         * Method that provides the answer of how many words in the sample text the client provided...
+         * Counts the number of words in the provided message.
          * 
-         * @param message
-         * @return words.length
+         * The method takes a string message as input, trims any leading or trailing 
+         * whitespaces, and splits the message based on whitespace characters to count 
+         * the words present in the message. The word count is determined by the number 
+         * of non-empty segments obtained after splitting the message.
+         *
+         * @param message The message in which words are counted.
+         * @return The count of words present in the message.
          */
         private int countWords(String message) {
         	// Gets message trims ending whitespaces and then counts all the remaining white spaces for word count
